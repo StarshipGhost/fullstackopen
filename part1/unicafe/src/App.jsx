72b3b55932aa6ade330cc97695a5beb49bd153b7
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2025-06-08 18:18:48 -0400

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,75 +1,103 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Header = ({ text }) => {
-  return (
-    <h1>{text}</h1>
-  )
-}
+  return <h1>{text}</h1>;
+};
 
 const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+const Grades = (props) => {
+  const grades = props.grades;
+
+  return grades.map(({ grade, count }) => (
+    <div key={grade}>
+      {grade} {count}
+    </div>
+  ));
+};
+
+const Total = (props) => {
   return (
-    <button onClick={props.onClick}>{props.text}</button>
-  )
-}
+    <div>
+      all {props.grades.map(({ count }) => count).reduce((a, b) => a + b)}
+    </div>
+  );
+};
 
-const DisplayStatistics = (props) => {
-  const statistics = props.statistics
+const Average = (props) => {
+  const grades = props.grades;
+  const total = grades.map(({ count }) => count).reduce((a, b) => a + b);
+  return (
+    <div>
+      average{" "}
+      {grades.map(({ count, value }) => count * value).reduce((a, b) => a + b) /
+        total}
+    </div>
+  );
+};
 
-  return ( 
-    statistics.map(({grade, value}) => <div>{grade} {value}</div>)
-  )
-}
 
+const Positive = (props) => {
+  const grades = props.grades;
+  const goodGradeCount = grades[0].count;
+  const total = grades.map(({ count }) => count).reduce((a, b) => a + b);
 
+  return <div>positive {(goodGradeCount / total) * 100} %</div>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-
-  const header = 'give feedback'
-  const statistics = [
+  const header = "give feedback";
+  const grades = [
     {
-      grade: 'good',
-      value: good
+      grade: "good",
+      count: good,
+      value: 1,
     },
     {
-      grade: 'neutral',
-      value: neutral
+      grade: "neutral",
+      count: neutral,
+      value: 0,
     },
     {
-      grade: 'bad',
-      value: bad
-    }
-  ]
-
+      grade: "bad",
+      count: bad,
+      value: -1,
+    },
+  ];
 
   const HandleGoodClick = () => {
     setGood(good + 1);
-  }
+  };
 
   const HandleNeutralClick = () => {
     setNeutral(neutral + 1);
-  }
+  };
 
   const HandleBadClick = () => {
     setBad(bad + 1);
-  }
-
-
+  };
 
   return (
     <div>
       <Header text={header} />
-      <Button onClick={HandleGoodClick} text='good' />
-      <Button onClick={HandleNeutralClick} text='neutral' />
-      <Button onClick={HandleBadClick} text='bad' />
-      <br /><br />
-      <DisplayStatistics statistics={statistics}/>
+      <Button onClick={HandleGoodClick} text="good" />
+      <Button onClick={HandleNeutralClick} text="neutral" />
+      <Button onClick={HandleBadClick} text="bad" />
+      <br />
+      <br />
+      <Grades grades={grades} />
+      <Total grades={grades} />
+      <Average grades={grades} />
+      <Positive grades={grades} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
