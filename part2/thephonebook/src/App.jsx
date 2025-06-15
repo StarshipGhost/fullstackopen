ab21d1a2856210dd8f1b52d50728d2abd6daa224
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2025-06-15 10:29:10 -0400

    Exercise 2.8

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
 
+const initialPersons = [{ name: "Arto Hellas", number: "040-1234567" }];
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState(initialPersons);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
-    if (!persons.map(({ name }) => name).includes(newName)) {
+    event.preventDefault();
+    if (newName.length === 0) {
+      alert(`You entered an invalid name or number`);
+    } else if (persons.map(({ name }) => name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
       const newPerson = {
         name: newName,
+        number: newNumber,
       };
       setPersons(persons.concat(newPerson));
-    } else {
-      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
     }
-    event.preventDefault();
-    setNewName("");
   };
 
   const nameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const numberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -29,12 +40,17 @@ const App = () => {
           name: <input value={newName} onChange={nameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={numberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <div key={name}>{name}</div>
+      {persons.map(({ name, number }) => (
+        <div key={name}>
+          {name} {number}
+        </div>
       ))}
     </div>
   );
